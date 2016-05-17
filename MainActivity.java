24eb2a53273b7d47cf2217d8430ec8cb1adf4b71
adriailla <adriailla01@gmail.com>
2016-05-17 16:12:48 +0200

    Create MainActivity.java

---
+++ MainActivity.java
@@ -0,0 +1,163 @@
+package com.example.android.navigationdrawer;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.NavigationView;
+import android.support.design.widget.Snackbar;
+import android.support.v4.app.Fragment;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity
+        implements NavigationView.OnNavigationItemSelectedListener, FragmentCamera.OnFragmentInteractionListener {
+
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.setDrawerListener(toggle);
+        toggle.syncState();
+
+        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
+    }
+
+    @Override
+    public void onBackPressed() {
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+
+            Intent intent = new Intent(MainActivity.this, Settings.class);
+            startActivity(intent);
+
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @SuppressWarnings("StatementWithEmptyBody")
+    @Override
+    public boolean onNavigationItemSelected(MenuItem item) {
+        // Handle navigation view item clicks here.
+        int id = item.getItemId();
+
+        boolean FragmentTransaction = false;
+        Fragment fragment = null;
+
+
+        //Camera
+
+        if (id == R.id.nav_camera) {
+
+            Intent intent = new Intent("android.media.action.IMAGE_CAPTURE");
+            startActivity(intent);
+
+            ImageView mImageView = (ImageView) findViewById(R.id.Galley1);
+
+
+
+        //Galley
+
+        } else if (id == R.id.nav_gallery) {
+
+            fragment = new FragmentCamera();
+            FragmentTransaction = true;
+
+        //Slideshow
+        } else if (id == R.id.nav_slideshow) {
+
+            Toast.makeText(getApplicationContext(), "No photos to slideshow", Toast.LENGTH_SHORT).show();
+
+         //Manage
+
+        } else if (id == R.id.nav_manage) {
+
+            Intent intent = new Intent(MainActivity.this, Settings.class);
+            startActivity(intent);
+
+
+        //Share
+
+        } else if (id == R.id.nav_share) {
+
+            Toast.makeText(getApplicationContext(), "Trying to share...", Toast.LENGTH_SHORT).show();
+
+
+         //Send
+
+        } else if (id == R.id.nav_send) {
+
+            Toast.makeText(getApplicationContext(), "Couldn't get file to send", Toast.LENGTH_SHORT).show();
+
+        }
+
+        if(FragmentTransaction) {
+            getSupportFragmentManager().beginTransaction()
+                    .replace(R.id.drawer_layout, fragment)
+                    .commit();
+
+            item.setChecked(true);
+            getSupportActionBar().setTitle(item.getTitle());
+        }
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
+    }
+
+    @Override
+    public void onFragmentInteraction(Uri uri) {
+
+
+    }
+}
